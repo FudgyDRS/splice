28c7d3a06b47f744633fbb9f7f0213d8a96089c0
FudgyDRS <yetimonsters@gmail.com>
2023-01-03 14:48:43 -0800

    new generator protocol

---
+++ projects/protocol/ll_generator/contracts/StateERC721.sol
@@ -0,0 +1,23 @@
+//function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool)
+
+//string private _name;
+//string private _symbol;
+
+//mapping(uint256 => address) private _owners;
+//mapping(address => uint256) private _balances;
+//mapping(uint256 => address) private _tokenApprovals;
+//mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+//function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool)
+//function balanceOf(address owner) public view virtual override returns (uint256)
+//function ownerOf(uint256 tokenId) public view virtual override returns (address)
+//function name() public view virtual override returns (string memory)
+//function symbol() public view virtual override returns (string memory)
+//function tokenURI(uint256 tokenId) public view virtual override returns (string memory)
+//function approve(address to, uint256 tokenId) public virtual override
+//function getApproved(uint256 tokenId) public view virtual override returns (address)
+//function setApprovalForAll(address operator, bool approved) public virtual override
+//function isApprovedForAll(address owner, address operator) public view virtual override returns (bool)
+//function transferFrom(address from, address to, uint256 tokenId) public virtual override
+//function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override
+//function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public virtual override
