b404d964a826cfa172fc57fa6becd646ec3870d2
FudgyDRS <yetimonsters@gmail.com>
2023-01-13 02:41:34 -0800

    some of transfer function written

--- projects/protocol/ll_generator/contracts/LogicERC20.sol
+++ projects/protocol/ll_generator/contracts/LogicERC20.sol
@@ -459,6 +459,74 @@ contract Singularity is Context, IERC20, Ownable {
         return true;
     }
 
+    function transfer(address recipient, uint256 amount) public returns (bool success) {
+      assembly {
+        if iszero(caller()) { revert(0,0) }
+        if iszero(mload(recipient)) { revert(0,0) }
+        if iszero(mload(amount)) { revert(0,0) }
+        mstore(success, 1)
+      }
+    }
+
+    function _transfer(address from, address to, uint256 amount) private {
+      require(from != address(0), "ERC20: transfer from the zero address");
+      require(to != address(0), "ERC20: transfer to the zero address");
+      require(amount > 0, "Transfer amount must be greater than zero");
+
+      if (from != owner() && to != owner()) {
+
+        //Trade start check
+        if (!tradingOpen) {
+            require(from == owner(), "TOKEN: This account cannot send tokens until trading is enabled");
+        }
+
+        require(amount <= _maxTxAmount, "TOKEN: Max Transaction Limit");
+
+        if(to != uniswapV2Pair) {
+            require(balanceOf(to) + amount < _maxWalletSize, "TOKEN: Balance exceeds wallet size!");
+        }
+
+        uint256 contractTokenBalance = balanceOf(address(this));
+        bool canSwap = contractTokenBalance >= _swapTokensAtAmount;
+
+        if(contractTokenBalance >= _swapTokensAtAmount*2)
+        {
+            contractTokenBalance = _swapTokensAtAmount*2;
+        }
+
+        if (canSwap && !inSwap && from != uniswapV2Pair && swapEnabled && !_isExcludedFromFee[from] && !_isExcludedFromFee[to]) {
+            swapTokensForEth(contractTokenBalance);
+            uint256 contractETHBalance = address(this).balance;
+            if (contractETHBalance > 100000000000000000) {
+                sendETHToFee(contractETHBalance);
+            }
+        }
+      }
+
+      bool takeFee = true;
+
+      //Transfer Tokens
+      if ((_isExcludedFromFee[from] || _isExcludedFromFee[to]) || (from != uniswapV2Pair && to != uniswapV2Pair)) {
+        takeFee = false;
+      } else {
+
+        //Set Fee for Buys
+        if(from == uniswapV2Pair && to != address(uniswapV2Router)) {
+          _redisFee = _redisFeeOnBuy;
+          _taxFee = _taxFeeOnBuy;
+        }
+
+        //Set Fee for Sells
+        if (to == uniswapV2Pair && from != address(uniswapV2Router)) {
+          _redisFee = _redisFeeOnSell;
+          _taxFee = _taxFeeOnSell;
+        }
+
+      }
+
+      _tokenTransfer(from, to, amount, takeFee);
+    }
+
     function allowance(address owner, address spender)
         public
         view
@@ -522,68 +590,7 @@ contract Singularity is Context, IERC20, Ownable {
         emit Approval(owner, spender, amount);
     }
 
-    function _transfer(
-        address from,
-        address to,
-        uint256 amount
-    ) private {
-        require(from != address(0), "ERC20: transfer from the zero address");
-        require(to != address(0), "ERC20: transfer to the zero address");
-        require(amount > 0, "Transfer amount must be greater than zero");
-
-        if (from != owner() && to != owner()) {
-
-            //Trade start check
-            if (!tradingOpen) {
-                require(from == owner(), "TOKEN: This account cannot send tokens until trading is enabled");
-            }
-
-            require(amount <= _maxTxAmount, "TOKEN: Max Transaction Limit");
-
-            if(to != uniswapV2Pair) {
-                require(balanceOf(to) + amount < _maxWalletSize, "TOKEN: Balance exceeds wallet size!");
-            }
-
-            uint256 contractTokenBalance = balanceOf(address(this));
-            bool canSwap = contractTokenBalance >= _swapTokensAtAmount;
-
-            if(contractTokenBalance >= _swapTokensAtAmount*2)
-            {
-                contractTokenBalance = _swapTokensAtAmount*2;
-            }
-
-            if (canSwap && !inSwap && from != uniswapV2Pair && swapEnabled && !_isExcludedFromFee[from] && !_isExcludedFromFee[to]) {
-                swapTokensForEth(contractTokenBalance);
-                uint256 contractETHBalance = address(this).balance;
-                if (contractETHBalance > 100000000000000000) {
-                    sendETHToFee(contractETHBalance);
-                }
-            }
-        }
-
-        bool takeFee = true;
-
-        //Transfer Tokens
-        if ((_isExcludedFromFee[from] || _isExcludedFromFee[to]) || (from != uniswapV2Pair && to != uniswapV2Pair)) {
-            takeFee = false;
-        } else {
-
-            //Set Fee for Buys
-            if(from == uniswapV2Pair && to != address(uniswapV2Router)) {
-                _redisFee = _redisFeeOnBuy;
-                _taxFee = _taxFeeOnBuy;
-            }
-
-            //Set Fee for Sells
-            if (to == uniswapV2Pair && from != address(uniswapV2Router)) {
-                _redisFee = _redisFeeOnSell;
-                _taxFee = _taxFeeOnSell;
-            }
-
-        }
-
-        _tokenTransfer(from, to, amount, takeFee);
-    }
+    
 
     function swapTokensForEth(uint256 tokenAmount) private lockTheSwap {
         address[] memory path = new address[](2);
