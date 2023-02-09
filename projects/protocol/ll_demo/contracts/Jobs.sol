4f6ca88fd34524f2313f514003fdd905f5b5039c
FudgyDRS <yetimonsters@gmail.com>
2023-02-09 04:00:43 -0800

    thoughts

--- projects/protocol/ll_demo/contracts/Jobs.sol
+++ projects/protocol/ll_demo/contracts/Jobs.sol
@@ -1,2 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+// if external call reverts with error message, does it count as an event on the external contract
+// can you get a revert event on an external contract without reverting the origin transaction
+
+/*
+contracts A and B
+
+A calls B
+  B reverts with special error (with data)
+  A completes execution
+*/
